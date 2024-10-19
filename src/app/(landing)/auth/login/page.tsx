129ab97c1d601524f900ef6253c34b19f149dce9
Lakshmi Sankar <plsankar1996@gmail.com>
2024-10-20 00:16:23 +0530

    fix lint errors

--- src/app/(landing)/auth/login/page.tsx
+++ src/app/(landing)/auth/login/page.tsx
@@ -3,7 +3,7 @@ import { authOptions } from "@/lib/auth/options";
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 
-export default async function LoginPage(request: Request) {
+export default async function LoginPage() {
     const session = await getServerSession(authOptions);
     if (session) {
         redirect("/dashboard");
